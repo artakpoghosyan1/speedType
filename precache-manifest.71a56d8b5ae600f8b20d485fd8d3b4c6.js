385e71212398d2e350ca68b6307644088cf49f0c
artakpoghosyan <artakpoghosyan@workfront.com>
2020-06-07 12:08:20 +0400

    Updates

---
+++ precache-manifest.71a56d8b5ae600f8b20d485fd8d3b4c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1b42e0938d7e22f35318a75685e933",
+    "url": "/speedType/index.html"
+  },
+  {
+    "revision": "d39b86cfa11e1f17bb97",
+    "url": "/speedType/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d39b86cfa11e1f17bb97",
+    "url": "/speedType/static/js/2.47988243.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/speedType/static/js/2.47988243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d9cb068cd43b70e9bc",
+    "url": "/speedType/static/js/main.522e059a.chunk.js"
+  },
+  {
+    "revision": "a67bd6e69ceb78113a65",
+    "url": "/speedType/static/js/runtime-main.d1c9af3d.js"
+  }
+]);
